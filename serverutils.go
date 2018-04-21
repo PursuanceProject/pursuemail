e7060285ad50a6ce1114d8fa73ed4dbe7ca5a7d7
Steve Phillips <steve@tryingtobeawesome.com>
2018-04-21 02:21:37 -0700

    Fixed buggy concurrent SendBulkEmail() (would hang on error)

--- serverutils.go
+++ serverutils.go
@@ -9,28 +9,38 @@ import (
 
 func SendBulkEmail(emailAccounts []*EmailAccount, sendBulkEmailReq *SendBulkEmailRequest, emailPool *emailLib.Pool) (failedIds []string) {
 	failedIdsChan := make(chan string)
+	var total int
 	wg := new(sync.WaitGroup)
-	for _, email := range emailAccounts {
+	for i, email := range emailAccounts {
 		if sendBulkEmailReq.SecureOnly && !email.HasPubKey() {
 			failedIds = append(failedIds, email.Id)
 			continue
 		}
 
+		total++
 		wg.Add(1)
-		go func(email *EmailAccount) {
-			defer wg.Done()
+		go func(i int, email *EmailAccount) {
+			var maybeFailedId string
+			log.Debugf("Sending bulk email #%v", i+1)
 			err := email.Send(sendBulkEmailReq.EmailData, emailPool)
 			if err != nil {
-				log.Errorf("Error sending email: %v", err)
-				failedIdsChan <- email.Id
+				log.Errorf("Error sending (instance of bulk) email: %v", err)
+				maybeFailedId = email.Id
 			}
-		}(email)
+			wg.Done()
+			failedIdsChan <- maybeFailedId
+		}(i, email)
 	}
 
+	log.Debugf("SendBulkEmailRequest: waiting for %v email(s) to send", total)
 	wg.Wait()
-	close(failedIdsChan)
-	for failedId := range failedIdsChan {
-		failedIds = append(failedIds, failedId)
+
+	var failedId string
+	for i := 0; i < total; i++ {
+		failedId = <-failedIdsChan
+		if failedId != "" {
+			failedIds = append(failedIds, failedId)
+		}
 	}
 
 	return failedIds
